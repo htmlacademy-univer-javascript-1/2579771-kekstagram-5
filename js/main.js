7fc18e26ebeaa1842cf10ef7be3fd007767a8215
Алексей Селезнев <a.seleznev@g.nsu.ru>
2024-12-25 18:34:46 +0700

    Task 9 initail commit

--- js/main.js
+++ js/main.js
@@ -1,14 +1,44 @@
 import { renderPhotos } from "./photoRender.js";
-import { generatePhotos } from "./data.js";
+import { fetchImages } from "./loadImages.js";
+import { initializeValidation } from "./formValidation.js";
+import { handleSubmit } from "./formSubmit.js";
 
 /**
- * Функция инициализации процесса генерации фотографий и добавления фотографий в DOM-дерево.
- * Процесс: Генерация данных фотографий -> Рендеринг фотографий
- * (Поиск необходимых элементов в дереве, добавление сгненрированных данных, добавлние слушателей событий для элементов)
+ * Инициализация процесса загрузки и отображения фотографий.
+ * @async
+ */
+async function initializePhotos() {
+  try {
+    const photos = await fetchImages();
+    if (photos) {
+      console.log("Данные подгружены");
+      renderPhotos(photos);
+    } else {
+      console.warn("Сервер недоступен. Используем тестовые данные.");
+      // const testPhotos = generatePhotos();
+      // renderPhotos(testPhotos);
+    }
+  } catch (error) {
+    console.error("Ошибка инициализации фотографий:", error);
+  }
+}
+
+/**
+ * Инициализация формы загрузки.
+ */
+function initializeForm() {
+  const uploadForm = document.querySelector(".img-upload__form");
+  const pristine = initializeValidation(uploadForm);
+
+  handleSubmit(uploadForm, pristine);
+}
+
+/**
+ * Основная инициализация приложения.
  */
 function init() {
-  const photos = generatePhotos();
-  renderPhotos(photos);
+  initializePhotos();
+  initializeForm();
 }
 
 init();
